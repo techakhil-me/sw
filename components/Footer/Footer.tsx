42a856bedde537e265b836e99fb686c1454b4553
techakhil-me <techakhilc47@gmail.com>
2023-11-09 23:10:01 +0530

    fixed icon

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
       <div className={styles.footerContainer}>
         <div className={`app-max-width app-x-padding gap-20 ${styles.footerContents}`}>
           <div className="flex items-center flex-col text-center">
-            <svg aria-hidden="true" focusable="false" fill="none" strokeWidth="1.5" width="24" className="hidden sm:block icon icon-picto-delivery-truck" viewBox="0 0 24 24">
+            <svg aria-hidden="true" focusable="false" fill="none" strokeWidth="1.5" width="24" viewBox="0 0 24 24">
               <path d="M23.25 13.5V6a1.5 1.5 0 0 0-1.5-1.5h-12A1.5 1.5 0 0 0 8.25 6v6m0 0V6h-3a4.5 4.5 0 0 0-4.5 4.5v6a1.5 1.5 0 0 0 1.5 1.5H3" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
               <path d="M.75 12h3a1.5 1.5 0 0 0 1.5-1.5V6" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
               <path clipRule="evenodd" d="M7.5 19.5a2.25 2.25 0 1 0 0-4.5 2.25 2.25 0 0 0 0 4.5Zm12 0a2.25 2.25 0 1 0 0-4.5 2.25 2.25 0 0 0 0 4.5Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
@@ -27,7 +27,7 @@ export default function Footer() {
             </div>
           </div>
           <div className="flex items-center flex-col text-center">
-            <svg aria-hidden="true" focusable="false" fill="none" strokeWidth="1.5" width="24" className="hidden sm:block icon icon-picto-award-gift" viewBox="0 0 24 24">
+            <svg aria-hidden="true" focusable="false" fill="none" strokeWidth="1.5" width="24"  viewBox="0 0 24 24">
               <path clipRule="evenodd" d="M15.75 23.238a3 3 0 0 0-3-3H9a3 3 0 0 0-3-3H.75v6h15Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
               <path d="M6 20.238h3m2.25-3H21a.75.75 0 0 0 .75-.75v-6.75m-13.5 0v4.5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
               <path clipRule="evenodd" d="M6.75 6.738a.75.75 0 0 1 .75-.75h15a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-.75.75h-15a.75.75 0 0 1-.75-.75v-2.25Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
@@ -41,7 +41,7 @@ export default function Footer() {
           </div>
 
           <div className="flex items-center flex-col text-center">
-          <svg aria-hidden="true" focusable="false" fill="none" strokeWidth="1.5" width="24" className="hidden sm:block icon icon-picto-plane" viewBox="0 0 24 24">
+          <svg aria-hidden="true" focusable="false" fill="none" strokeWidth="1.5" width="24" viewBox="0 0 24 24">
       <path clipRule="evenodd" d="m20.055 20.198.654-.654c.36-.36.502-.885.373-1.378l-2.326-8.837 3.075-3.075a2.972 2.972 0 0 0 .422-3.794 2.867 2.867 0 0 0-4.369-.37l-3.183 3.185L5.867 2.95a1.434 1.434 0 0 0-1.38.373l-.653.654A1.434 1.434 0 0 0 4.11 6.22l6.03 3.618-4.589 5.2-1.434.02a1.434 1.434 0 0 0-1.225.37L1.46 16.74a.716.716 0 0 0 .225 1.165l2.767 1.56 1.816 2.864a.718.718 0 0 0 1.166.224l1.251-1.193c.354-.333.515-.822.428-1.3l.023-1.438 5.058-4.73 3.618 6.03a1.434 1.434 0 0 0 2.243.276Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
     </svg>
             <h3 className={styles.footerHead}>EXPRESS DELIVERY</h3>
